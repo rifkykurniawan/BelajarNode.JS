f8a6e317076f61e5480eafb0d5a7435ec7407c2b
rifkykurniawan <toettoet.tt@gmail.com>
2021-05-29 11:22:20 +0700

    P11- GET and DELETE Images with Postman

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -7,10 +7,50 @@ const isBase64Img = require("base64-img");
 
 const{Media} = require("../models")
 
+const fs = require("fs");
+
+/* GET ALL Data Image*/
+router.get("/", async (req, res) => { 
+  const media = await Media.findAll({ 
+    attributes: ["id", "image"], 
+  }); 
+  const mapedMedia = media.map((m) => { 
+    m.image = `${req.get("host")}/${m.image}`; 
+    return m; 
+  }); 
+  return res.json({ 
+    status: "success", 
+    data: media, 
+  }); 
+}); 
+
+/* Delete Image */
+router.delete("/:id", async (req, res) => { 
+  const id = req.params.id; 
+  const media = await Media.findByPk(id); 
+  if (!media) { 
+    return res.status(400).json({ 
+      status: "error", message: "media not found" 
+    }); 
+  } 
+  fs.unlink(`./public/${media.image}`, async (err) => { 
+    if (err) { 
+      return res.status(400).json({ status: "error", message: err.message }); 
+    } 
+
+    await media.destroy(); 
+    return res.json({ 
+      status: "Success", 
+      message: "image deleted", 
+    }); 
+  }); 
+  
+});
+
 /* Upload Image. */
 router.post("/", (req, res) => {
   const image = req.body.image;
-
+  
   if (!isBase64(image, { mimeRequired: true })) {
     res.status(400).json({ status: "error", message: "invalid base64 image" });
   }
@@ -22,7 +62,7 @@ router.post("/", (req, res) => {
     }
     
     const filename = filepath.split("\\").pop().split("/").pop(); //windows
-
+    
     const media = await Media.create({ image: `images/${filename}` });
     
     return res.json({
